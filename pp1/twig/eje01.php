c1b8cecd93e0964773ca89410be26f31f150e766
Felipe Florio <ffelipe.flo@gmail.com>
2026-08-11 17:52:43 -0300

    Eje 01 u 7 (11/08/2026)

--- pp1/twig/eje01.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once './vendor/autoload.php';
-$loader = new \Twig\Loader\FilesystemLoader('./templates');
-$twig = new \Twig\Environment($loader);
-echo $twig->render('eje01.html.twig', ['nombre' => $_GET['name']]);
-?> 
--- pp1/twig/eje02.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once './vendor/autoload.php';
-$loader = new \Twig\Loader\FilesystemLoader('./templates');
-$twig = new \Twig\Environment($loader);
-echo $twig->render('eje02.html.twig', ['nombre' => $_GET['name']]);
-?> 
